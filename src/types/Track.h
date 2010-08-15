f76ade14fdbd5618a3d829370308d687260bb7bc
Leo Franchi <lfranchi@kde.org>
2010-08-14 21:43:04 -0400

    Add types/ and stub

---
+++ src/types/Track.h
@@ -0,0 +1,38 @@
+/****************************************************************************************
+ * Copyright (c) 2010 Leo Franchi <lfranchi@kde.org>                                    *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+
+#ifndef ECHONEST_TRACK_H
+#define ECHONEST_TRACK_H
+
+#include <QSharedDataPointer>
+
+namespace Echonest 
+{
+
+class TrackData;
+  
+LIBECHONEST_EXPORT class Track
+{
+  
+  
+private:
+  QSharedDataPointer<TrackData> d;
+};
+  
+} // namespace
+
+#endif
---
+++ src/types/Track_p.h
@@ -0,0 +1,47 @@
+/****************************************************************************************
+ * Copyright (c) 2010 Leo Franchi <lfranchi@kde.org>                                    *
+ *                                                                                      *
+ * This program is free software; you can redistribute it and/or modify it under        *
+ * the terms of the GNU General Public License as published by the Free Software        *
+ * Foundation; either version 2 of the License, or (at your option) any later           *
+ * version.                                                                             *
+ *                                                                                      *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY      *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.             *
+ *                                                                                      *
+ * You should have received a copy of the GNU General Public License along with         *
+ * this program.  If not, see <http://www.gnu.org/licenses/>.                           *
+ ****************************************************************************************/
+
+#ifndef LIBECHONEST_TRACK_P_H
+#define LIBECHONEST_TRACK_P_H
+
+#include <QSharedData>
+
+namespace Echonest
+{
+  
+
+class TrackData : QSharedData
+{
+  TrackData() {}
+  ~TrackData() {}
+  TrackData( const TrackData& other ) : QSharedData( other ) {}
+  
+  QString artistName;
+  QString title;
+  QString analysisURL;
+  uint trackId;
+  int mode;
+  QString previewUrl;
+  QString releaseName;
+  /** TODO analyisStatus */
+  qreal tempo;
+  qreal loudness;
+  uint timeSignature;
+};
+  
+}
+
+#endif
