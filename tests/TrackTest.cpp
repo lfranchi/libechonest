20888c081cf4fff321b5f3ceac27b4b77cbcd15d
Leo Franchi <lfranchi@kde.org>
2012-07-18 17:21:30 -0400

    Update copyright

--- tests/TrackTest.cpp
+++ tests/TrackTest.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************************
- * Copyright (c) 2010 Leo Franchi <lfranchi@kde.org>                                    *
+ * Copyright (c) 2010-2012 Leo Franchi <lfranchi@kde.org>                               *
  * Copyright (c) 2011 Jeff Mitchell <mitchell@kde.org>                                  *
  *                                                                                      *
  * This program is free software; you can redistribute it and/or modify it under        *
