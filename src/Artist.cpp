36e76c05c30f680403a4122722170d9bae2e7f4a
Stefan Derkits <stefan@derkits.at>
2013-01-10 17:03:21 +0100

    use string comparison again, adding a boolean could compromise binary compatibility

--- src/Artist.cpp
+++ src/Artist.cpp
@@ -373,7 +373,7 @@ QNetworkReply* Echonest::Artist::fetchSongs( int numResults, int offset  ) const
 
 QNetworkReply* Echonest::Artist::fetchTerms( Echonest::Artist::TermSorting sorting ) const
 {
-    QUrl url = setupQuery( "terms", 0, -1, false );
+    QUrl url = setupQuery( "terms", 0, -1 );
     if( sorting == Echonest::Artist::Weight )
         url.addEncodedQueryItem( "sort", "weight" );
     else if( sorting == Echonest::Artist::Frequency )
@@ -399,7 +399,7 @@ QNetworkReply* Echonest::Artist::fetchVideo(int numResults, int offset) const
 
 QNetworkReply *Echonest::Artist::fetchGenres() const
 {
-    QUrl url = setupQuery( "list_genres", 0, -1, false );
+    QUrl url = setupQuery( "list_genres", 0, -1 );
 
     return Echonest::Config::instance()->nam()->get( QNetworkRequest( url ) );
 }
@@ -556,14 +556,14 @@ QVector< QString > Echonest::Artist::parseGenreList( QNetworkReply* reply ) thro
     return genres;
 }
 
-QUrl Echonest::Artist::setupQuery( const QByteArray& methodName, int numResults, int start, bool requiresArtist = true ) const
+QUrl Echonest::Artist::setupQuery( const QByteArray& methodName, int numResults, int start ) const
 {
     QUrl url = Echonest::baseGetQuery( "artist", methodName );
     if( !d->id.isEmpty() )
         url.addEncodedQueryItem( "id", d->id );
     else if( !d->name.isEmpty() ) {
         url.addEncodedQueryItem( "name", Echonest::escapeSpacesAndPluses( d->name ) );
-    } else if (requiresArtist) {
+    } else if ( methodName != "list_genres" || methodName != "terms" ) {
         qWarning() << "Artist method" << methodName << "called on an artist object without name or id!";
         return QUrl();
     }
